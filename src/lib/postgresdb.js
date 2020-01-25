34a549a87beb033900df72560bb9ed7e1bdfa009
Dave Perez <kitsunebrave@gmail.com>
2020-01-25 00:22:14 -0500

    delay time got pg handler retried multiple times

--- src/lib/postgresdb.js
+++ src/lib/postgresdb.js
@@ -3,55 +3,64 @@
 const secretsFetch = require('./index.js');
 const opts = {
     awsSdkOptions: {
-        region: 'us-east-2'
+        region: 'us-east-1'
     },
     cache: true,
     cacheExpiryInMillis: Math.floor(Date.now() * 5 * 60000),
-    secrets: ['pgdev']//these secrets are arbitrary for now
-}
-
-try {
-    const res = secretsFetch(opts).init();
-    if(res) console.log(`Secrets successfully loaded at ${Date.now()}`)
-} catch (error) {
-    console.log(error)
+    secrets: ['pgCred', 'gisdb_ma']//these secrets are arbitrary for now
 }
 
 const { Pool } = require('pg');
 
-let pgPool;
-const poolInitializer = async () => {
-    if(!pgPool) {
-        const pgCred = JSON.parse(global.cacheSecrets.pgCred);
-        pgPool = new Pool({
-            user: pgCred.user,
-            password: pgCred.password,
-            database: pgCred.dbname,
-            host: pgCred.host,
-            port: pgCred.port,
-            max: 20,
-            idleTimeoutMillis: 1000,
-            connectionTimeoutMillis: 1000
+const poolInitializer = () => {
+    return new Promise((resolve, reject) => {
+        secretsFetch(opts).init()
+        .then(secrets => {
+            if(secrets) console.log(`Secrets successfully loaded at ${Date.now()}`)
+            console.log('secrets loaded::', global.cacheSecrets)
+            const pgCred = global.cacheSecrets.pgCred;
+            let pgPool = new Pool({
+                username: pgCred.username,
+                password: pgCred.password,
+                database: pgCred.dbname,
+                host: pgCred.host,
+                port: pgCred.port,
+                max: 20,
+                idleTimeoutMillis: 1000,
+                connectionTimeoutMillis: 1000
+            });
+            pgPool.on('error', (err, client) => {
+                console.log('RDS Postgress Database Error Encountered', client, err);
+                process.exit(1);
+            });
+            
+            resolve(pgPool);
         })
-        pgPool.on('error', (err, client) => {
-            console.log('RDS Postgress Database Error Encountered', client, err);
-            process.exit(1);
+        .catch(error =>{
+            console.log('Pool error', error)
+            reject(error)
         })
-    }
 
-    return pgPool;
+    })
 }
 
 const query = async queryObject => {
     let dbResponse;
+    let retry_pool = false,
+        retry_count = 0;
     try {
-        await poolInitializer();
+        let pgPool = await poolInitializer();
         dbResponse = await pgPool.query(queryObject);
     } catch (error) {
-        if(error.code === '28P01' && retry_pool === false) {
-            throw Error('Authentication failed')
+        if(error.code === '28P01' && retry_pool === false) {   
+            retry_count = retry_count + 1;
+            delay = Math.pow(2) * 3000; //constraints for every postgres sleep time connections
+            await new Promise(resolve => setTimeout(resolve, delay));
+            await query(queryObject);
         }
-        throw error;
+        
+        if(retry_count > 3)
+            throw Error('Retry has been unsuccessful, check AWS-SecretsManager manager for any failover');
     }
 
     return dbResponse;
